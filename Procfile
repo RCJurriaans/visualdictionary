1a59d251f7f10793b2b02190e6d6f2a50a427a27
RCJurriaans <robrechtjurriaans@gmail.com>
2015-02-16 11:43:14 +0100

    Initial set-up

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn visualdict.wsgi --log-file -
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/robrecht/Documents/Perceptum/visualdictionary/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
